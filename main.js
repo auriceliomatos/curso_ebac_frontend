2167c8485b938a4124a849aa1be4c0a3949093f5
#Auricelio_Matos <auriceliomatos@gmail.com>
2024-12-07 10:26:11 -0300

    basico

--- main.js
+++ main.js
@@ -41,38 +41,6 @@ try {
 }
 });
 
-
-// Example of fetching GitHub profile data using the Fetch API
-
-    // document.addEventListener('DOMContentLoaded', function() {
-    //     const nameElement = document.querySelector('#name');
-    //     const usernameElement = document.querySelector('#username');
-    //     const avatarElement = document.querySelector('#avatar');
-    //     const reposElement = document.querySelector('#repos');
-    //     const followersElement = document.querySelector('#followers');
-    //     const followingElement = document.querySelector('#following');
-    //     const linkElement = document.querySelector('#profile-link');
-    //     try {
-    //          fetch('https://api.github.com/users/maxh33')
-    //              .then(function(res) {
-    //                  if (!res.ok) {
-    //                      throw new Error('Network response was not ok ' + res.statusText);
-    //                  }
-    //                  return res.json();
-    //             })
-    //             .then(function(data) {
-    //                 if (nameElement) nameElement.innerText = data.name;
-    //                 if (usernameElement) usernameElement.innerText = data.login;
-    //                 if (avatarElement) avatarElement.src = data.avatar_url;
-    //                 if (followingElement) followingElement.innerText = data.following;
-    //                 if (followersElement) followersElement.innerText = data.followers;
-    //                 if (reposElement) reposElement.innerText = data.public_repos;
-    //                 if (linkElement) linkElement.href = data.html_url;
-    //             })
-    //             .catch(function(err) {
-    //                 console.error('Error fetching data:', err.message);
-    //             });
-    //      } catch (error) {
     //          console.error('Error fetching data:', error.message);
     //      }
     // });
