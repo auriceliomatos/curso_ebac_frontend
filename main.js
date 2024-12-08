4fa26237c85b89c0ab2b2b5cb27843fb7425a22d
auriceliomatos <auriceliomatos@gmail.com>
2024-12-08 10:53:26 -0300

    up no doc

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-// Example of fetching GitHub profile data using AJAX (XMLHttpRequest)
-
 document.addEventListener('DOMContentLoaded', function() {
-
     const nameElement = document.querySelector('#name');
     const usernameElement = document.querySelector('#username');
     const avatarElement = document.querySelector('#avatar');
@@ -11,36 +8,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const linkElement = document.querySelector('#link');
     const bioElement = document.querySelector('#bio');
     const companyElement = document.querySelector('#company');
-   
 
-try {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.github.com/users/auriceliomatos', true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
-
-            if (nameElement) nameElement.innerText = data.name;
-            if (usernameElement) usernameElement.innerText = data.login;
-            if (avatarElement) avatarElement.src = data.avatar_url;
-            if (followingElement) followingElement.innerText = data.following;
-            if (followersElement) followersElement.innerText = data.followers;
-            if (reposElement) reposElement.innerText = data.public_repos;
-            if (linkElement) linkElement.href = data.html_url;
-            if (bioElement )bioElement.innerText = data.bio;
-            if (companyElement) companyElement.innerText = data.company;
-            
-
-        } else if (xhr.readyState === 4) {
-            console.error('Error fetching data:', xhr.statusText);
-        }
-    };
-    xhr.send();
-} catch (error) {
-    console.error('Error fetching data:', error.message);
-}
-});
-
-    //          console.error('Error fetching data:', error.message);
-    //      }
-    // });
+    fetch('https://api.github.com/users/auriceliomatos')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(json) {
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followingElement.innerText = json.following;
+            followersElement.innerText = json.followers;
+            repos.innerText = json.public_repos;
+            linkElement.href = json.html_url;
+           bioElement.innerText = json.bio;
+            companyElement.innerText = json.company;
+        })
+        .catch(function(erro) {
+            alert("Um erro inesperado aconteceu no carregamento, tente novamente mais tarde.");
+        })
+})
